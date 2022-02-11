df98c17d280f0cdbdd7c2d9f7e8707cdfe2f427d
pereiraalvaro14 <pereiraalvaro@gmail.com>
2022-02-11 00:28:57 +0000

    Ejercicio carta CASI finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let suit = ["♦", "♥", "♠", "♣"];
+
+  let randomNumbers = Math.floor(Math.random() * numbers.length);
+  let randomSuit = Math.floor(Math.random() * suit.length);
+
+  document.getElementById("number").innerHTML = numbers[randomNumbers];
+  document.getElementById("suitUp").innerHTML = suit[randomSuit];
+  document.getElementById("suitDown").innerHTML = suit[randomSuit];
+  if (suit[randomSuit] == "♥" || suit[randomSuit] == "♦") {
+    document.getElementById("suitUp").className = "rojo";
+    document.getElementById("suitDown").className = "rojo";
+    document.getElementById("number").className = "rojo";
+  }
 };
